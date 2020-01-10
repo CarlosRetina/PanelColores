76d6c8c6dfe94470653e2d1f3cf9c3fd5e62399f
Carlos Peiro Embid <carlospeiroembid@gmail.com>
2020-01-10 18:32:53 +0100

    Initial Commit

---
+++ PanelColores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PanelColores
+//
+//  Created by Carlos Peiro Embid on 10/01/2020.
+//  Copyright © 2020 Carlos Peiro Embid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
